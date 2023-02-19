fb2adec906d9f1b4c5409d9912620f7ec3cd7ee3
Nikita <wescholm@gmail.com>
2023-02-20 00:14:35 +0200

    Initial commit

---
+++ libs/http-client/axios/http-client.ts
@@ -0,0 +1,47 @@
+import axios, { AxiosInstance } from "axios";
+import { HttpsProxyAgent } from "https-proxy-agent";
+import { Logger, LogLevel} from "../../logger";
+
+export interface IAxiosResponse {
+    data: any;
+    status: number;
+}
+
+export class HttpClient {
+
+    private readonly axios: AxiosInstance;
+    private readonly logger: Logger;
+
+    constructor(proxyUrl?: string) {
+        this.logger = new Logger({
+            level: LogLevel.INFO,
+            prefix: "HttpClient"
+        });
+        const validateStatus = () => false; // Don't throw on non-2xx responses
+        if (proxyUrl) {
+            const proxyAgent = new HttpsProxyAgent(proxyUrl);
+            this.axios = axios.create({
+                httpsAgent: proxyAgent,
+                httpAgent: proxyAgent,
+                validateStatus
+            });
+        } else {
+            this.axios = axios.create({ validateStatus });
+            this.logger.warn("Proxy not provided!");
+        }
+    }
+
+    public async get(url: string): Promise<IAxiosResponse> {
+        const response = await this.axios.get(url);
+        const { status, data } = response;
+        this.logger.debug(`GET ${url} ${status}`);
+        return { data, status };
+    }
+
+    public async post(url: string, payload: any): Promise<IAxiosResponse> {
+        const response = await this.axios.post(url, payload);
+        const { status, data } = response;
+        this.logger.debug(`POST ${url} ${status}`);
+        return { data, status };
+    }
+}
---
+++ libs/logger/index.ts
@@ -0,0 +1,70 @@
+import chalk from "chalk";
+
+export enum LogLevel {
+    ERROR = "error",
+    WARN = "warn",
+    INFO = "info",
+    DEBUG = "debug",
+}
+
+export interface LoggerOptions {
+    level: LogLevel;
+    prefix?: string
+}
+
+export class Logger {
+    private readonly level: LogLevel;
+    private readonly prefix: string;
+
+    constructor(options: LoggerOptions) {
+        this.level = options.level;
+        this.prefix = this.setPrefix(options.prefix);
+    }
+
+    log(level: LogLevel, message: string): void {
+        if (this.shouldLog(level)) {
+            const timestamp = new Date().toISOString();
+            const coloredLevel = this.getColor(level)(level.toUpperCase());
+            console.log(`${timestamp} [${coloredLevel}] ${this.prefix + message}`);
+        }
+    }
+
+    error(message: string): void {
+        this.log(LogLevel.ERROR, message);
+    }
+
+    warn(message: string): void {
+        this.log(LogLevel.WARN, message);
+    }
+
+    info(message: string): void {
+        this.log(LogLevel.INFO, message);
+    }
+
+    debug(message: string): void {
+        this.log(LogLevel.DEBUG, message);
+    }
+
+    private shouldLog(level: LogLevel): boolean {
+        return LogLevel[level] <= LogLevel[this.level];
+    }
+
+    private getColor(level: LogLevel): (message: string) => string {
+        switch (level) {
+            case LogLevel.ERROR:
+                return chalk.red;
+            case LogLevel.WARN:
+                return chalk.yellow;
+            case LogLevel.INFO:
+                return chalk.green;
+            case LogLevel.DEBUG:
+                return chalk.blue;
+            default:
+                return chalk.white;
+        }
+    }
+
+    private setPrefix(prefix?: string): string {
+        return prefix ? `[${prefix}] ` : "";
+    }
+}
